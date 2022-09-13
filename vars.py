29801512630da6d7f5e89979f4b2365f3b810c64
cwkuo <albert.cwkuo@gatech.edu>
2022-09-13 15:47:27 -0400

    update blacklist nodes

--- vars.py
+++ vars.py
@@ -1,5 +1,5 @@
 blacklist = [
-    'jill', 'hal', 'ash', 'calculon', 'c3po', 'breq', 'eva', 'johnny5', 'neo', 'dave', 'claptrap', 'oppy', 'bmo', 'chappie', 'cortana'
+    'jill', 'hal', 'ash', 'calculon', 'c3po', 'breq', 'eva', 'johnny5', 'neo', 'bmo', 'xaea-12', 'cortana'
 ]
 
 gpu_types = ["rtx_6000", "2080_ti", "a40", "titan_x"]
